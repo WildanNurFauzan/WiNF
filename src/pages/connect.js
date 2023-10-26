868bc13eb0b825c6fd7c85e36938ed01307d1a21
WildanNurFauzan <leadergalaxybleh@gmail.com>
2023-10-25 23:03:02 -0500

    dhhwsb

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -2,6 +2,8 @@ import React from "react";
 import Head from "next/head";
 import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
+import contactImg from "../../public/images/svg/contact-img.svg";
+import Image from "next/image";
 
 const connect = () => {
   return (
@@ -11,16 +13,24 @@ const connect = () => {
         <meta name="description" content="any description" />
       </Head>
       <main>
-        <Layout flex >
-          
-          <div>
+        <Layout flex>
+          <AnimatedText text="Haha" className="flex justify-center mb-16" />
 
-            sjhgshshshs
-            
-             
-              
+          <div className="flex w-full bg-dark h-auto ">
+            <div className="w-[48rem] h-250  ">
+              <Image src={contactImg} alt="Gambar" className="w-full" />
+            </div>
+            <div className="flex flex-col w-[24rem] h-auto">
+              <div className="flex bg-slate-600 ml-8 w-full h-auto  ">
+                <div className="px-[48px] pt-16 border ">Name</div>
+                <div className="px-[48px] pt-16">sks</div>
+              </div>
+              <div className="flex bg-slate-600 ml-8 w-full h-auto ">
+                <div className="px-[48px] pt-16">Name</div>
+                <div className="px-[48px] pt-16">sks</div>
+              </div>
+            </div>
           </div>
-          <AnimatedText text="Haha" />
         </Layout>
       </main>
     </>
